4fc1a47e8ce2c82d85d233712f34a4bf394f2eba
vaibhav rathod <vaibhavrr18@gmail.com>
2022-12-14 13:04:46 +0530

    Models Migrated to Database

---
+++ BankManagementApi/Migrations/20221214073125_CreatingModels.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankManagementApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatingModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomerMaster",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNo = table.Column<long>(type: "bigint", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerMaster", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerBalanceInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Balance = table.Column<long>(type: "bigint", nullable: false),
+                    AccountType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerBalanceInfo", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CustomerBalanceInfo_CustomerMaster_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "CustomerMaster",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerLoyaltyPoints",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Points = table.Column<int>(type: "int", nullable: false),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    AccountId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerLoyaltyPoints", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CustomerLoyaltyPoints_CustomerBalanceInfo_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "CustomerBalanceInfo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerBalanceInfo_CustomerId",
+                table: "CustomerBalanceInfo",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerLoyaltyPoints_AccountId",
+                table: "CustomerLoyaltyPoints",
+                column: "AccountId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerLoyaltyPoints");
+
+            migrationBuilder.DropTable(
+                name: "CustomerBalanceInfo");
+
+            migrationBuilder.DropTable(
+                name: "CustomerMaster");
+        }
+    }
+}
