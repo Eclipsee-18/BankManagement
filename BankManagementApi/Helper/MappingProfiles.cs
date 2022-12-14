89532612d2f2b6a8a18a784385fba1d010b204a3
vaibhav rathod <vaibhavrr18@gmail.com>
2022-12-14 15:07:55 +0530

    Mapping Profile

---
+++ BankManagementApi/Helper/MappingProfiles.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BankManagementApi.Dto;
+using BankManagementApi.Models;
+
+namespace BankManagementApi.Helper
+{
+	public class MappingProfiles:Profile
+	{
+		public MappingProfiles()
+		{
+			CreateMap<CustomerMaster, CustomerMasterDto>();
+			CreateMap<CustomerMasterDto, CustomerMaster>();
+			CreateMap<CustomerBalanceInfo, CustomerBalanceInfoDto>();
+			CreateMap<CustomerBalanceInfoDto, CustomerBalanceInfo>();
+			CreateMap<CustomerLoyaltyPoints, CustomerLoyaltyPointsDto>();
+			CreateMap<CustomerLoyaltyPointsDto, CustomerLoyaltyPoints>();
+		}
+		
+	}
+}
--- BankManagementApi/Program.cs
+++ BankManagementApi/Program.cs
@@ -1,4 +1,6 @@
 using BankManagementApi.Data;
+using BankManagementApi.Interfaces;
+using BankManagementApi.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IMasterRepository, MasterRepository>();
+builder.Services.AddScoped<IBalanceInfoRepository, BalanceInfoRepository>();
+builder.Services.AddScoped<ILoyaltyPointsRepository, LoyaltyPointsRepository>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
